7bbf08e5076501448b5ca54b4537c42f7b3f59b0
Adel Kobic <adelthekid@yahoo.com>
2025-04-15 02:55:30 +0200

    .

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/slick-portfolio-svelte-5';
+const dev = process.env.NODE_ENV !== 'production';
+const base = dev ? '' : '/slick-portfolio-svelte-5';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ fallback: '404.html' }),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html', // This becomes important for SPA mode
+      precompress: false
+    }),
+    paths: {
+      base: base
+    }
+  }
 };
 
 export default config;
